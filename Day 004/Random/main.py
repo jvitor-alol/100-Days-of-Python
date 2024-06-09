68b4d3a89d98be3a49f57e1365ef0a9d8b790310
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-06-09 16:05:20 -0300

    Day 4: Lists

--- Day 004/Random/main.py
+++ Day 004/Random/main.py
@@ -15,6 +15,31 @@ def func_1() -> None:
     print(random_number)
 
 
+def func_2() -> None:
+    states_of_us = ["Delaware", "Pennsylvania", "New Jersey", "Georgia",
+                    "Connecticut", "Massachusetts", "Maryland",
+                    "South Carolina", "New Hampshire", "Virginia", "New York",
+                    "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+                    "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi",
+                    "Illinois", "Alabama", "Maine", "Missouri", "Arkansas",
+                    "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+                    "California", "Minnesota", "Oregon", "Kansas",
+                    "West Virginia", "Nevada", "Nebraska", "Colorado",
+                    "North Dakota", "South Dakota", "Montana", "Washington",
+                    "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico",
+                    "Arizona", "Alaska", "Hawaii"]
+    dirty_dozen = [
+        "Strawberries", "Spinach", "Kale", "Nectarines", "Apples",
+        "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes",
+        "Celery", "Potatoes"]
+
+    # random.seed(1000)
+    index = random.randint(0, 49)
+
+    print(f"{states_of_us[index]} - ", end='')
+    print(dirty_dozen[random.randint(0, len(dirty_dozen) - 1)])
+
+
 def main() -> None:
     parse = argparse.ArgumentParser()
     parse.add_argument('-o', '--option', type=int, choices=range(2),
@@ -23,6 +48,8 @@ def main() -> None:
 
     if args.option == 0:
         func_1()
+    if args.option == 1:
+        func_2()
 
 
 if __name__ == '__main__':
