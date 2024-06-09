f1feccea8e5dfce53028ddc1819a0da0e866532f
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-06-08 21:50:39 -0300

    Day 3: BMI 2.0

--- Day 003/Control Flow/main.py
+++ Day 003/Control Flow/main.py
@@ -2,6 +2,29 @@
 import argparse
 
 
+def bmi_2() -> None:
+    # Enter your height in meters e.g., 1.55
+    height = float(input())
+    # Enter your weight in kilograms e.g., 72
+    weight = int(input())
+    # 🚨 Don't change the code above 👆
+
+    # Write your code below this line 👇
+    bmi = weight / height ** 2
+    message = "Your BMI is {}, you {}."
+
+    if bmi < 18.5:
+        print(message.format(bmi, "are underweight"))
+    elif bmi < 25:
+        print(message.format(bmi, "have a normal weight"))
+    elif bmi < 30:
+        print(message.format(bmi, "are slightly overweight"))
+    elif bmi < 35:
+        print(message.format(bmi, "are obese"))
+    else:
+        print(message.format(bmi, "are clinically obese"))
+
+
 def even_or_odd() -> None:
     #  Which number do you want to check?
     number = int(input())
@@ -34,13 +57,15 @@ def rollercoaster() -> None:
 def main() -> None:
     parse = argparse.ArgumentParser()
     parse.add_argument("-o", "--option", type=int,
-                       choices=range(2), default=0, help="choose function")
+                       choices=range(3), default=0, help="choose function")
     args = parse.parse_args()
 
     if args.option == 0:
         rollercoaster()
     if args.option == 1:
         even_or_odd()
+    if args.option == 2:
+        bmi_2()
 
 
 if __name__ == '__main__':
