bacd342f916d3d1be536bf7bbfc290e55f905fcb
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-06-10 13:35:15 -0300

    Day 5: Adding Even Numbers

---
+++ Day 005/Adding Even Numbers/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def main() -> None:
+    target = int(input())  # Enter a number between 0 and 1000
+    # 🚨 Do not change the code above ☝️
+
+    # Write your code here 👇
+    even_sum = 0
+    for num in range(2, target + 1, 2):
+        even_sum += num
+    print(even_sum)
+
+
+if __name__ == '__main__':
+    main()
