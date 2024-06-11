794785285e8acaba3bbc51fae469b5a6b755cde4
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-06-11 15:30:37 -0300

    Day 6: Reeborg's World - Hurdle 2

---
+++ Day 006/hurdle_2.py
@@ -0,0 +1,23 @@
+# Reebord's World Hurdle 2:
+from .reeborgs_world import turn_left, move, at_goal
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    jump_hurdle()
