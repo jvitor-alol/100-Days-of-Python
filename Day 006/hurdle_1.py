516f7899f765944c35e8d9a2349e4366a2f430d2
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-06-11 15:30:18 -0300

    Day 6: Reeborg's World - Hurdle 1

---
+++ Day 006/hurdle_1.py
@@ -0,0 +1,23 @@
+# Reebord's World Hurdle 1:
+from .reeborgs_world import turn_left, move
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+for _ in range(6):
+    jump_hurdle()
---
+++ Day 006/reeborgs_world.py
@@ -0,0 +1,23 @@
+# Reeborg's default functions:
+def turn_left() -> None:
+    ...
+
+
+def move() -> None:
+    ...
+
+
+def at_goal() -> bool:
+    ...
+
+
+def front_is_clear() -> bool:
+    ...
+
+
+def wall_in_front() -> bool:
+    ...
+
+
+def wall_on_right() -> bool:
+    ...
