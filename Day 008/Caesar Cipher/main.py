983a84272c79376327adc8be5820dad937828dcd
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-06-13 19:01:22 -0300

    Improvements to Caesar Cipher

--- Day 008/Caesar Cipher/main.py
+++ Day 008/Caesar Cipher/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import sys
 import os.path
+import argparse
 from random import shuffle
 
 from art import text2art
 from unidecode import unidecode
+from termcolor import colored
 
 from check_input import check_input
 
@@ -16,16 +18,16 @@ ALPHABET = [
     '5', '6', '7', '8', '9', '0']
 
 
-def cipher_key_generator() -> list[str]:
+def cipher_key_generator(filepath: str = CIPHER_PATH) -> list[str]:
     cipher_key: list[str] = []
 
-    if not os.path.isfile(CIPHER_PATH):
+    if not os.path.isfile(filepath):
         shuffle(ALPHABET)
         cipher_key = ALPHABET
-        with open(CIPHER_PATH, 'w', encoding='utf-8') as file:
+        with open(filepath, 'w', encoding='utf-8') as file:
             file.write(' '.join(cipher_key))
     else:
-        with open(CIPHER_PATH, mode='r', encoding='utf-8') as file:
+        with open(filepath, mode='r', encoding='utf-8') as file:
             content = file.readlines()
             cipher_key = content[0].strip().split(' ')
 
@@ -48,23 +50,56 @@ def caesar_cipher(direction: str, plain_text: str, shift: int,
     return ''.join(cipher_text)
 
 
-def main() -> None:
+def main(parse: argparse.ArgumentParser) -> None:
     print(text2art("Caesar Cipher"))
 
-    key = cipher_key_generator()
+    args = parse.parse_args()
+    key = cipher_key_generator(args.key)
 
-    direction = check_input(
-        "Type 'encode' to encrypt / 'decode' to decrypt: ",
-        lambda x: x in ['encode', 'decode'])
-    text = unidecode(input("Type your message: ").lower())
-    shift = int(check_input("Type the shift number: ", lambda x: x.isdigit()))
+    if args.mode:
+        direction = args.mode
+    else:
+        direction = check_input(
+            "Type 'encode' to encrypt / 'decode' to decrypt: ",
+            lambda x: x in ['encode', 'decode'])
+
+    if args.text:
+        text = args.text.lower()
+    else:
+        text = unidecode(input("Type your message: ").lower())
+
+    if args.shift:
+        shift = args.shift
+    else:
+        shift = int(check_input(
+            "Type the shift number: ", lambda x: x.isdigit()))
 
     cipher_text = caesar_cipher(direction, text, shift, key)
-    print(f"\n{direction.capitalize()}d message is:\n" + cipher_text)
+    print(f"\n{direction.capitalize()}d message is:")
+    print(colored(cipher_text, "green", attrs=["blink"]))
+    sys.exit()
 
 
 if __name__ == '__main__':
+    parse = argparse.ArgumentParser(
+        description="""
+        Encrypts or decrypts messages using the Caesar cipher method.""")
+
+    parse.add_argument(
+        '-m', '--mode', type=str, choices=['encode', 'decode'],
+        help="Selects the cipher mode: "
+        "'encode' to encrypt or 'decode' to decrypt.")
+    parse.add_argument(
+        '-s', '--shift', type=int,
+        help="Specifies a number for the shift amount of the Caesar cipher.")
+    parse.add_argument(
+        '-t', '--text', type=str,
+        help="Plain text message to encrypt or decrypt.")
+    parse.add_argument(
+        '-k', '--key', type=str, default='./key.txt',
+        help="Path to cipher key in the system.")
+
     try:
-        main()
+        main(parse)
     except KeyboardInterrupt:
         sys.exit(1)
