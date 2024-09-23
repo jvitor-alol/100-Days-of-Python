a90ba87c31ae5c22622c967fda3b758cd7618112
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-09-23 19:33:01 -0300

    Day 25: More Testing with Pandas

--- Day 025/main.py
+++ Day 025/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
-import csv
+# import csv
 
+import pint
 import pandas as pd
+import numpy as np
 
 CSV_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -12,22 +14,41 @@ CSV_PATH = os.path.join(
 
 def main() -> None:
     # with builtin csv module
-    with open(CSV_PATH) as csvfile:
-        data = csv.reader(csvfile)
+    # with open(CSV_PATH) as csvfile:
+    #     data = csv.reader(csvfile)
 
-        next(data)  # skip the csv header in iterator
-        temperatures = [int(row[1]) for row in data]
+    #     next(data)  # skip the csv header in iterator
+    #     temperatures = [int(row[1]) for row in data]
 
-        # for row in data:
-        #     print(row)
+    #     # for row in data:  # iterator already consumed by list comprehension
+    #     #     print(row)
 
-        print(temperatures)
+    #     print(temperatures)
 
     # with pandas
     data = pd.read_csv(CSV_PATH, header='infer')
     print(data)
-    print(data.dtypes)
-    print(data['temp'])
+    # print(data.dtypes)
+    # print(data['temp'])
+
+    data_dict = data.to_dict()
+    print(data_dict)
+
+    # avg_temp = data['temp'].mean()  # alternative
+    temperatures = data['temp'].to_list()
+    avg_temp = np.mean(temperatures)
+    print(round(avg_temp, 2))
+
+    print(data[data['temp'] == data['temp'].max()])
+
+    print(convert_to_farenheit(data[data.day == 'Monday']['temp'][0]))
+
+
+def convert_to_farenheit(temperature: float) -> float:
+    ureg = pint.UnitRegistry()
+    temperature_celsius = temperature * ureg.degC
+
+    return round(temperature_celsius.to('degF'), 1)
 
 
 if __name__ == '__main__':
