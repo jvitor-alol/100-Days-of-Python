172a1e4f9a68547289fdcbaadab2ee46af854efa
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2024-09-27 19:57:34 -0300

    Day 26: EPs 15-17 Done

--- Day 026/List Comprehension/main.py
+++ Day 026/List Comprehension/main.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
+from os.path import join, dirname, abspath
 from argparse import ArgumentParser
 
+FILE_1_TXT = join(dirname(abspath(__file__)), 'file1.txt')
+FILE_2_TXT = join(dirname(abspath(__file__)), 'file2.txt')
 
-def square_numbers() -> None:
-    raise NotImplementedError
+
+def square_numbers(numbers: list[int]) -> None:
+    squared_numbers = [n ** 2 for n in numbers]
+    print(squared_numbers)
 
 
-def filter_even_numbers() -> None:
-    raise NotImplementedError
+def filter_even_numbers(list_of_strings: list[str]) -> None:
+    numbers = [int(n) for n in list_of_strings]
+    result = [n for n in numbers if n % 2 == 0]
+    print(result)
+
+    # one-liner
+    # result = list(filter(lambda n: n % 2 == 0, map(int, list_of_strings)))
+    # print(result)
 
 
 def data_overlap() -> None:
-    raise NotImplementedError
+    with open(FILE_1_TXT) as file1, open(FILE_2_TXT) as file2:
+        a_list = list(map(int, file1.readlines()))
+        b_list = list(map(int, file2.readlines()))
+
+    result = [num for num in a_list if num in b_list]
+
+    print(result)
 
 
 def main() -> None:
@@ -24,9 +41,10 @@ def main() -> None:
 
     match args.option:
         case 0:
-            square_numbers()
+            square_numbers([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
         case 1:
-            filter_even_numbers()
+            filter_even_numbers(
+                ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99'])
         case 2:
             data_overlap()
         case _:
